30be4c96c32a171010c461fcd97522796095f46c
Pengge Zhuang <1064480036@qq.com>
2017-09-13 01:05:36 +0800

    add:初始化项目

--- Application/Index/Controller/IndexController.php
+++ Application/Index/Controller/IndexController.php
@@ -1,8 +1,18 @@
 <?php
+//本api才用百度音乐api
+//可以查看Public/readme.md
 class IndexController extends Controller {
+
+    private $listUrl = "http://tingapi.ting.baidu.com/v1/restserver/ting";
     public function indexAction() {
-        $this->view->assign("test","hello world!");
         $this->view->show();
+    }
+    //获取列表
+    public function listAction() {
+        $data = $_GET;
+        $this->load('Common/Function');
+        $data = http($this->listUrl,$data);
+        echo $data;die;
     }
 }
 ?>
