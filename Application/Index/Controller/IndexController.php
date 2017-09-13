f01fac8db866a5de6d57cfbb245a0c5e1d6936c3
Pengge Zhuang <1064480036@qq.com>
2017-09-13 11:57:14 +0800

    add:添加验证类以及完善音乐接口

--- Application/Index/Controller/IndexController.php
+++ Application/Index/Controller/IndexController.php
@@ -4,14 +4,30 @@
 class IndexController extends Controller {
 
     private $listUrl = "http://tingapi.ting.baidu.com/v1/restserver/ting";
+    private $downloadUrl = "http://music.baidu.com/data/music/fmlink";
+
+    public function __construct(){
+
+        parent::__construct();
+    }
     public function indexAction() {
         $this->view->show();
     }
     //获取列表
     public function listAction() {
         $data = $_GET;
-        $this->load('Common/Function');
         $data = http($this->listUrl,$data);
+        echo $data;die;
+    }
+    //歌曲url
+    public function downloadAction() {
+        $data = $_GET;
+        $validate_params = array(
+            'rate#int',
+            'songIds#int',
+        );
+        $validate_data = $this->validate->check($validate_params, $data);
+        $data = http($this->downloadUrl,$validate_data);
         echo $data;die;
     }
 }
--- Library/Sys/Controller.php
+++ Library/Sys/Controller.php
@@ -7,15 +7,17 @@
 class Controller
 {
     private $loadException  = "FrameException";
+    protected $validate;
     private $hasException   = false;
     protected $view;
     public static $session = array();
     public function __CONSTRUCT()
     {
 
-        //默认导入TestException异常类
-        $this->load('Exception/'.$this->loadException);
-        $this->load('Common/ErrorCode');
+        //默认导入异常类,验证类以及公共方法
+        $loadArr = array('Exception/'.$this->loadException,'Common/Validate','Common/Function','Common/ErrorCode');
+        $this->load($loadArr);
+        $this->validate = new Validate();
         //设置异常处理函数
         restore_exception_handler();
         set_exception_handler(array($this,"setExceptionHandler"));
@@ -31,14 +33,17 @@ class Controller
     }
     public function setExceptionHandler(Throwable $e = null) {
         $this->hasException = true;
-        $this->ShowErrorHtml($e);
+        if(strpos($_SERVER['HTTP_HOST'],'api') !== false) {
+            $this->showExceptionApi($e);
+        }
+        $this->ShowExceptionHtml($e);
 
     }
 
     /**
      * @desc 显示错误信息
      */
-    private function ShowErrorHtml(Throwable $e = null) {
+    private function ShowExceptionHtml(Throwable $e = null) {
         $html = <<< HTML
         <pre class="alert alert-danger" style="color:#a94442;background-color: #f2dede;border-color: #ebccd1;font-size: 15px">
         <h2 class="error-danger" style="text-align:center">出错了</h2>
@@ -51,6 +56,18 @@ HTML;
     public function showError($msg, $code = -1) {
         $code   = $msg->getCode();
         $desc   = $msg->getMessage();
+        $return = array(
+            'e' =>  array(
+                'code'  => $code,
+                'desc'  => $desc
+            )
+        );
+        echo json_encode($return);die;
+
+    }
+    public function showExceptionApi(Throwable $e = null) {
+        $code   = $e->getCode();
+        $desc   = $e->getMessage();
         $return = array(
             'e' =>  array(
                 'code'  => $code,
